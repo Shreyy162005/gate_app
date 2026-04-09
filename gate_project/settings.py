ab5335be646367ab1006b6312e5b952511737e3a
Shreya Pawar <rp@gmail.com>
2026-04-09 17:43:56 +0530

    fixed allowed hosts

--- gate_project/settings.py
+++ gate_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g1k%^a8qtvl$g6_np(watutcmqz@bi=u=wzgus=bk1vh8r0%(v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
